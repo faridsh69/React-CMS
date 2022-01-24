8b2c42203bc45280fb33afef13c972c10bf2449b
farid.sh69@gmail.com <farid.sh69@gmail.com>
2022-01-24 10:00:54 +0330

    List of tasks completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Routes>
+          <Route path="/" element={<input />} />
+          <Route path="about" element={<input type="checkbox" />} />
+        </Routes>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
